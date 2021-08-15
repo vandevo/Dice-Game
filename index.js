8c181ebf74b5decb159385239420cbb864e6be3b
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-08-16 00:06:40 +0100

    mastert

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ let player1Score = 0
 let player2Score = 0
 let player1Turn = true
 
-let message = document.getElementById('message')
+const message = document.getElementById('message')
 
-let player1Scoreboard = document.getElementById('player1Scoreboard')
-let player2Scoreboard = document.getElementById('player2Scoreboard')
+const player1Scoreboard = document.getElementById('player1Scoreboard')
+const player2Scoreboard = document.getElementById('player2Scoreboard')
 
-let player1Dice = document.getElementById('player1Dice')
-let player2Dice = document.getElementById('player2Dice')
+const player1Dice = document.getElementById('player1Dice')
+const player2Dice = document.getElementById('player2Dice')
 
-let rollBtn = document.getElementById('rollBtn')
-let resetBtn = document.getElementById('resetBtn')
+const rollBtn = document.getElementById('rollBtn')
+const resetBtn = document.getElementById('resetBtn')
 
-console.log('rollBtn');
