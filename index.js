a5af1ff4cdef94c526d793b2599501aadb499a84
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-08-15 22:51:31 +0100

    commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 let player1Score = 0
 let player2Score = 0
 let player1Turn = true
+
+let message = document.getElementById('message')
+
+let player1Scoreboard = document.getElementById('player1Scoreboard')
+let player2Scoreboard = document.getElementById('player2Scoreboard')
+
+let player1Dice = document.getElementById('player1Dice')
+let player2Dice = document.getElementById('player2Dice')
+
+let rollBtn = document.getElementById('rollBtn')
+let resetBtn = document.getElementById('resetBtn')
+
+console.log('rollBtn');
