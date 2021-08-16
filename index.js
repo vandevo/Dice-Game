c978cff8957ba7670eabb397cf3c19c1eaf9e4b2
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-08-17 00:00:41 +0100

    master

--- index.js
+++ index.js
@@ -18,12 +18,17 @@ rollBtn.addEventListener('click', function(){
     console.log(randomNumber);
 
     if (player1Turn) {
+       
         player1Dice.textContent = randomNumber;
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
         player1Dice.classList.remove("active")
         player2Dice.classList.add("active")
         message.textContent = "Player 1 Roll Now!"
     } else {
         player2Dice.textContent = randomNumber
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
         player2Dice.classList.remove("active")
         player1Dice.classList.add("active")
         message.textContent = "Player 2 Roll Now!"
